2469edf9d82974c3449ffdafd74a72d4b5086f8f
Daniyar0210 <162981018+Daniyar0210@users.noreply.github.com>
2024-06-29 21:19:08 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+# бот, который считает, сколько разлагается вещь
+
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+#Код
+items = {
+    "пластик" : 450,
+    "стекло"  : "не разлогаймый",
+    "полителен" : 20,
+    "алюминий" : 318,
+    "подгузник" : 500,
+}
+
+
+@bot.command()
+async def howlon(ctx, item):
+    if item in items:
+        await ctx.send(f"Вещь {item} разлагается за {items[item]} лет")
+    else:
+        await ctx.send("Такой вещи нет")
+
+@bot.command()
+async def eco(ctx):
+    facts = [
+        "Каждый год в океаны попадает около 8 миллионов тонн пластика.",
+        "Леса покрывают около 30% поверхности земли и являются домом для 80% наземных видов животных и растений.",
+        "Более миллиарда людей живут без доступа к чистой питьевой воде.",
+        "Один рециклированный стеклянный бутыль экономит достаточно энергии, чтобы запитать лампу на 4 часа.",
+        "Каждое сохраненное дерево может поглотить около 21 кг углекислого газа в год.",
+    ]
+    await ctx.send(f"Рандомный факт : random.choice(facts)")
+bot.run("ТОКЕН")
